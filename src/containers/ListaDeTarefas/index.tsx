dbc7a6fc2750d128d07783dcc242b97228991c72
Horther <77111164+Horther@users.noreply.github.com>
2025-10-21 18:40:34 +0100

    first commit

---
+++ src/containers/ListaDeTarefas/index.tsx
@@ -0,0 +1,28 @@
+import { useSelector } from 'react-redux'
+
+import Tarefa from '../../components/Tarefa'
+import { Container } from './styles'
+import { RootReducer } from '../../store'
+
+const ListaDeTarefas = () => {
+  const { tarefas } = useSelector((state: RootReducer) => state)
+  return (
+    <Container>
+      <p>2 Tarefas marcadas como: &quot;categoria&quot; e &quot;termo&quot;</p>
+      <ul>
+        {tarefas.map((t) => (
+          <li key={t.titulo}>
+            <Tarefa
+              descricao={t.descricao}
+              prioridade={t.prioridade}
+              status={t.status}
+              titulo={t.titulo}
+            />
+          </li>
+        ))}
+      </ul>
+    </Container>
+  )
+}
+
+export default ListaDeTarefas
